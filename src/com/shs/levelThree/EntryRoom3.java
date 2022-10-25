768f7859d123ff15548e3835786683173711ab59
hangilk <hangilxk24@sherwoodsd.com>
2022-10-25 12:12:26 -0700

    EntryRoom3 corrupted code theme implementation

--- src/com/shs/levelThree/EntryRoom3.java
+++ src/com/shs/levelThree/EntryRoom3.java
@@ -3,7 +3,6 @@ package com.shs.levelThree;
 import com.shs.RoomTemplate;
 
 public class EntryRoom3 extends RoomTemplate{
-
     public EntryRoom3() {
         super("System.out.print(\"An empty room.\")", "System.out.print(\"It's the level three starting room!\")");
         setMoveErrorMessage("setMoveErrorMessage(\"You can't go there.\")");
