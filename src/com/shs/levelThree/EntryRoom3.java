b844a3495852b190a78314923fc024d51c1a0154
hangilk <hangilxk24@sherwoodsd.com>
2022-10-25 12:49:44 -0700

    Differing indentation in EntryRoom3 and SecondRoom3

--- src/com/shs/levelThree/EntryRoom3.java
+++ src/com/shs/levelThree/EntryRoom3.java
@@ -4,7 +4,10 @@ import com.shs.RoomTemplate;
 
 public class EntryRoom3 extends RoomTemplate{
     public EntryRoom3() {
-        super("System.out.print(\"An empty room.\")", "System.out.print(\"It's the level three starting room!\")");
+        super(
+                "System.out.print(\"An empty room.\")",
+                "System.out.print(\"It's the level three starting room!\")"
+        );
         setMoveErrorMessage("setMoveErrorMessage(\"You can't go there.\")");
     }
 }
