9e1b145623db31304ef0e259426fa77fc6382af4
hangilk <hangilxk24@sherwoodsd.com>
2022-10-25 12:32:26 -0700

    2 new classes; Class & Method (lvl3)

---
+++ src/com/shs/item/Class.java
@@ -0,0 +1,20 @@
+package com.shs.item;
+
+import com.shs.ItemTemplate;
+
+public class Class extends ItemTemplate {
+    public Class() {
+        super("Class", "A class.", "An equipped class.");
+    }
+
+    @Override
+    public boolean isMoveable() {
+        return false;
+    }
+
+    @Override
+    public boolean isGettable() {
+        return true;
+    }
+
+}
