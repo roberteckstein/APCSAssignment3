fd5ae2a90af069a14baa8049a09a7a956732e496
hangilk <hangilxk24@sherwoodsd.com>
2022-10-26 11:16:48 -0700

    Adding text formatting to SecondRoom3.java

--- src/com/shs/levelThree/SecondRoom3.java
+++ src/com/shs/levelThree/SecondRoom3.java
@@ -6,8 +6,8 @@ public class SecondRoom3 extends RoomTemplate {
 
     public SecondRoom3() {
         super(
-                "This is the second room.",
-                "This room is very bright although you don't see a light sources. You see a passage heading east."
+                "System.out.print(\"This is the second room.\")",
+                "System.out.print(\"This room is very bright although you don't see a light source. You see a passage heading east.\")"
         );
     }
 }
