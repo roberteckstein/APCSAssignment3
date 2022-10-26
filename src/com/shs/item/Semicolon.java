6963a38bb822f0c7e35f0c70dd130f5907ebb966
trentondh25 <trentondh25@sherwoodsd.com>
2022-10-26 11:58:51 -0700

    Edited Semicolon object.

--- src/com/shs/item/Semicolon.java
+++ src/com/shs/item/Semicolon.java
@@ -6,4 +6,12 @@ public class Semicolon extends ItemTemplate {
     public Semicolon() {
         super("Semicolon", "A strange semicolon shaped object.", "A semicolon outside the computer!");
     }
+    @Override
+    public boolean isMoveable() {
+        return true;
+    }
+    @Override
+    public boolean isGettable() {
+        return true;
+    }
 }
