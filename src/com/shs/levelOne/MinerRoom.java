5f65994d52f5ad916f7e5d21178baf761935cd2c
thederpcodeman <connertm24@sherwoodsd.com>
2022-10-26 12:03:57 -0700

    added discription for miners room

--- src/com/shs/levelOne/MinerRoom.java
+++ src/com/shs/levelOne/MinerRoom.java
@@ -6,7 +6,7 @@ import com.shs.item.SmallStatue;
 
 public class MinerRoom extends RoomTemplate {
     public MinerRoom() {   // Why is there a '...' after String?
-        super("elemental room", "");
+        super("miner's room", "a small room with a miner and a crystal golem in each corner, to the west is an incredibly dark passage, the the east an open vault door, abandoned.");
 
         // Add items/creatures here (watch out for duplicate objects across rooms)
 
