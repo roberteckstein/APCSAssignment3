8494ba1479ec2877bd4437e676c8c701c5c6a378
thederpcodeman <connertm24@sherwoodsd.com>
2022-10-26 11:34:02 -0700

    Improved treasure room, fixed food, and added note

--- src/com/shs/levelOne/TreasureRoom.java
+++ src/com/shs/levelOne/TreasureRoom.java
@@ -6,5 +6,6 @@ public class TreasureRoom extends RoomTemplate {
     public TreasureRoom(){
         super("Vault","This room is filled wall to wall with expired food, there is an open door leading west");
         setMoveErrorMessage("There is no exit that way!");
+        addItem(new ExpiredFood());
     }
 }
