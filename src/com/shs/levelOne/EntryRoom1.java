5812500a537ec73faee0f840f7452ac32fddd639
GenericProgrammer <gddiamondnoob@gmail.com>
2022-10-26 16:16:45 -0700

    fixed error

--- src/com/shs/levelOne/EntryRoom1.java
+++ src/com/shs/levelOne/EntryRoom1.java
@@ -3,8 +3,6 @@ package com.shs.levelOne;
 import com.shs.RoomTemplate;
 
 public class EntryRoom1 extends RoomTemplate {
-
-    CrystalPouch pouch = new CrystalPouch();
     public EntryRoom1() {
         super("You are in the starting room. A passage travels north.", "You are in the starting room. A passage travels north.");
         setMoveErrorMessage("The room's walls are solid! right?");
