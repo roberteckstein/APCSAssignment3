2309d48f7c7d2e6c91f2e53a2eb8ee03d32a8a54
hangilk <hangilxk24@sherwoodsd.com>
2022-10-26 11:40:38 -0700

    Misc. refactoring

--- src/com/shs/levelOne/Room3.java
+++ src/com/shs/levelOne/Room3.java
@@ -7,5 +7,5 @@ public class Room3 extends RoomTemplate{
     public Room3() {
             super("Starting Room", "You are in the starting room. A passage travels north.");
             setMoveErrorMessage("The room's walls are solid!");
-        }
+    }
 }
