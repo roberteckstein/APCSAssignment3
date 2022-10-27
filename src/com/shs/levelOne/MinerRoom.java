38f4c1cac14632b6edb304daa97951329fe0917d
GenericProgram <gddiamondnoob@gmail.com>
2022-10-27 12:10:46 -0700

    did stuff

--- src/com/shs/levelOne/MinerRoom.java
+++ src/com/shs/levelOne/MinerRoom.java
@@ -1,15 +1,12 @@
 package com.shs.levelOne;
 
 import com.shs.RoomTemplate;
-import com.shs.item.Pickaxe;
 
 public class MinerRoom extends RoomTemplate {
-    Pickaxe pickaxe = new Pickaxe();
     public MinerRoom() {   // Why is there a '...' after String?
         super("You are in the miner's room.", "A pickaxe lies on the floor. A small room with a miner and a crystal golem in each corner, to the west is an incredibly dark passage, the the east an open vault door, abandoned.");
 
         // Add items/creatures here (watch out for duplicate objects across rooms)
-        addItem(pickaxe);
     }
 }
 
