fc1f07746d7778a00b5e73c0335163f15cb99c39
Tyler <tylerkn23@sherwoodsd.com>
2022-10-28 12:30:13 -0700

    added lvl 2 rooms

--- src/com/shs/levelOne/FinalRoom.java
+++ src/com/shs/levelOne/FinalRoom.java
@@ -51,7 +51,7 @@ public class FinalRoom extends RoomTemplate {
         }
         String result = d.use(i);
         if (!result.equals("")) {
-            TextAdventure.finalRoom.addPath("north", TextAdventure.entryRoom2);
+            TextAdventure.finalRoom.addPath("north", TextAdventure.one);
         }
         return result;
     }
