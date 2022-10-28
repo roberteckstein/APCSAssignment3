18b4eb737adf78f28a76e32198e28b9188c61e79
Archnitest <114258594+Archnitest@users.noreply.github.com>
2022-10-28 11:56:18 -0700

    Added Window.Java

---
+++ src/com/shs/item/Window.java
@@ -0,0 +1,36 @@
+package com.shs.item;
+
+
+import com.shs.RandomItemTemplate;
+
+public class Window extends RandomItemTemplate {
+    public Window () {
+        super ("window", "", "How did you get this?");
+        setDescription("There is a unopenable window on the wall. Behind the window is " + ranText(numGen(11)));
+
+    }
+    private String ranText (int x) {
+        switch (x) {
+            case 0:
+                return "a grassy field. ";
+            case 1:
+                return "the interior of a cave. ";
+            case 2:
+                return "a large kitchen. ";
+            case 3:
+                return "the side of a cliff overlooking a forest. ";
+            case 4:
+                return "yourself, staring back at you. ";
+            case 5:
+                return "the inside of a volcano. ";
+            case 6:
+                return "the middle of outer space. ";
+            case 7:
+                return "the room, flipped upside down. ";
+            case 8:
+                return "your deepest fear - something words cannot describe. ";
+            default:
+                return "A stone wall. ";
+        }
+    }
+}
