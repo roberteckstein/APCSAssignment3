00f48955bd88c1bb96964f248ac253fa7be16b5f
Archnitest <114258594+Archnitest@users.noreply.github.com>
2022-10-27 22:12:56 -0700

    Added InfiniteRoom.java documentation

--- src/com/shs/levelThree/InfiniteRoom.java
+++ src/com/shs/levelThree/InfiniteRoom.java
@@ -10,23 +10,33 @@ import java.util.Random;
 public class InfiniteRoom extends RoomTemplate {
     public InfiniteRoom () {
         super("This room looks familiar to you.","");
-        System.gc();
+        
+        System.gc(); // Calls Garbage Collecter due to the excess of unused objects being created.
+        
         setDescription("You are in a bedroom with doors North, West, East, South" + ranNoise(numGen(10)));
         setMoveErrorMessage("There may be many exits to this room, but that is not one of them.");
+        
+        // Adding Objects based on random number generator
         addItem(new Graffiti(ranColor(numGen(12))));
         if (numGen(10) > 4) {
             addItem(new Mattress(ranColor(numGen(12))));
         }
+        
+        // Adding paths to room. New instances of same InfiniteRoom Class
         addPath("north", new InfiniteRoom());
         addPath("south", new InfiniteRoom());
         addPath("east", new InfiniteRoom());
         addPath("west", new InfiniteRoom());
     }
+    
+    // Random Number Generator Method for reusabiity
     private int numGen (int high) {
         Random r = new Random();
         int x = r.nextInt(high);
         return(x);
     }
+    
+    // Random Noise Text Description to mess with user
     private String ranNoise (int x){
         String y  = "You hear a noise coming from the";
         switch (x) {
@@ -37,6 +47,8 @@ public class InfiniteRoom extends RoomTemplate {
             default : return "";
         }
     }
+    
+    // Random Color Stuff
     private String ranColor (int x){
         switch (x) {
             case 0 : return "blue";
