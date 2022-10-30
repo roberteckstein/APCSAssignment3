22306f64f18bcc58fe4d26929f4bb899f52c1353
Archnitest <114258594+Archnitest@users.noreply.github.com>
2022-10-30 09:43:40 -0700

    Simplified Code

--- src/com/shs/item/Class.java
+++ src/com/shs/item/Class.java
@@ -2,14 +2,10 @@ package com.shs.item;
 
 import com.shs.ItemTemplate;
 
+// Level 3 Item
 public class Class extends ItemTemplate {
     public Class() {
         super("class", "A class.", "An equipped class.");
-    }
-
-    @Override
-    public boolean isMoveable() {
-        return false;
     }
 
     @Override
