321f09aa9bbe0db428b0ca47eb1c80e40ac4413e
Tyler <tylerkn23@sherwoodsd.com>
2022-11-01 13:02:22 -0700

    added more stuff to lvl2

--- src/com/shs/levelTwo/SecondRoom2.java
+++ src/com/shs/levelTwo/SecondRoom2.java
@@ -3,6 +3,7 @@ package com.shs.levelTwo;
 import com.shs.ItemTemplate;
 import com.shs.TextAdventure;
 import com.shs.RoomTemplate;
+import com.shs.creature.Lava;
 import com.shs.item.Chest;
 import com.shs.item.Sunball;
 import com.shs.item.Sword;
@@ -11,14 +12,45 @@ import com.shs.item.FirePotion;
 
 
 public class SecondRoom2 extends RoomTemplate {
-    Chest chest = new Chest();
     FirePotion potion = new FirePotion();
+    Lava lava = new Lava();
     public SecondRoom2() {
         //Room description
-        super("", "You are in an empty room. There is a chest.");
+        super("Second Room", "You are in Hell.");
         setMoveErrorMessage("You can't go there.");
 
         // Add items/creatures here (watch out for duplicate objects across rooms)
-        addItem(chest);
+        addItem(lava);
+        addItem(potion);
+    }
+    @Override
+    public String getMoveErrorMessage(String direction) {
+        if (direction.equals("west") && lava.isAlive()) {
+            return "The passage to the west is blocked by lava in your way.";
+        } else {
+            return getMoveErrorMessage();
+        }
+    }
+    public String use(String target, String directObject)
+    {
+        ItemTemplate i = TextAdventure.playerInventory.getItem(target);
+        ItemTemplate d = getItem(directObject);
+
+
+        if (d == null) {
+            d = TextAdventure.playerInventory.getItem(directObject);
+        }
+
+        if (i == null) {
+            return "You do not have that item.";
+        } else if (d == null) {
+            return "You can't find that to use the item on.";
+        }
+        String result = d.use(i);
+        if (!result.equals(""))
+        {
+            TextAdventure.two.addPath("west", TextAdventure.four);
+        }
+        return result;
     }
 }
