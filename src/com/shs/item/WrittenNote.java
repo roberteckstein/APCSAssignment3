c170310f4af198dc468357fc8b5121f8dd68e799
hangilk <hangilxk24@sherwoodsd.com>
2022-11-01 12:49:12 -0700

    Text refactors

--- src/com/shs/item/WrittenNote.java
+++ src/com/shs/item/WrittenNote.java
@@ -5,10 +5,13 @@ import com.shs.ItemTemplate;
 public class WrittenNote extends ItemTemplate {
 
     public WrittenNote(){
-        super("note", "You see a scribbled note tacked to the wall.", "Complaint form 162: \nThe entrance is such a tripping hazard, \njust yesterday I tripped over a loose stone and almost fell on my pick, I tried to brace myself but it didn't work, it was like the wall wasn't there! \nMust have been farther away than I thought.");
+        super("note", "a note with small writing, you'd have to be holding it close to read it", "Complaint form 162    The entrace is such a tripping hazard, only yesterday i triped over a loose stone and almost fell on my pick, I tried to brace myself but it didn't work, it was like the wall wasn't there! must have been farther away than I thought.");
 
     }
     public boolean isGettable() {
             return true;
         }
 }
+
+
+
