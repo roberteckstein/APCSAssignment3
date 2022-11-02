ad12baef63f27d8267a61cf5e8446dde3363fa02
GenericProgram <gddiamondnoob@gmail.com>
2022-11-02 11:30:33 -0700

    started code on making firepotion make more sense

--- src/com/shs/levelTwo/SecondRoom2.java
+++ src/com/shs/levelTwo/SecondRoom2.java
@@ -18,11 +18,13 @@ public class SecondRoom2 extends RoomTemplate {
         //Room description
         super("Second Room", "You are in Hell.");
         setMoveErrorMessage("You can't go there.");
-
         // Add items/creatures here (watch out for duplicate objects across rooms)
         addItem(lava);
         addItem(potion);
+
     }
+
+
     @Override
     public String getMoveErrorMessage(String direction) {
         if (direction.equals("west") && lava.isAlive()) {
@@ -31,12 +33,11 @@ public class SecondRoom2 extends RoomTemplate {
             return getMoveErrorMessage();
         }
     }
-    public String use(String target, String directObject)
-    {
+
+
+    public String use(String target, String directObject) {
         ItemTemplate i = TextAdventure.playerInventory.getItem(target);
         ItemTemplate d = getItem(directObject);
-
-
         if (d == null) {
             d = TextAdventure.playerInventory.getItem(directObject);
         }
