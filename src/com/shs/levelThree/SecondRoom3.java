68c623f824f58d3da7ff7e393ddd38e0a929d8bc
Chris <chrisportinga1@gmail.com>
2022-11-03 09:49:27 -0700

    Setting up to present Lvl 3

--- src/com/shs/levelThree/SecondRoom3.java
+++ src/com/shs/levelThree/SecondRoom3.java
@@ -3,6 +3,7 @@ package com.shs.levelThree;
 import com.shs.ItemTemplate;
 import com.shs.RoomTemplate;
 
+
 import com.shs.item.*;
 
 public class SecondRoom3 extends RoomTemplate {
@@ -11,6 +12,7 @@ public class SecondRoom3 extends RoomTemplate {
     Doll redDoll = new Doll("red");
     Doll blueDoll = new Doll("blue");
     private String description = "Pacing around the room, something clicks as you step on a hidden button.\nThe floor opens up and you tumble through, into...\n\nA perfectly circular marble room. Almost identical to the last, but noticeably larger.";
+
     public SecondRoom3() {
         super(
                 "A circular marble room. Seems familiar.",
@@ -25,10 +27,13 @@ public class SecondRoom3 extends RoomTemplate {
     public String getLongDescription() {
 
         String returnValue = this.description;
-        for (ItemTemplate item: itemsInRoom.getInventory()) {
+        for (ItemTemplate item : itemsInRoom.getInventory()) {
             returnValue += "\n" + item.getDescription();
         }
 
         return returnValue;
+
+
     }
+
 }
