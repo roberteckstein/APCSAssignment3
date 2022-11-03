4042a28a139f050597e4121aa61b4b356e309175
hangilk <114253929+hangilk@users.noreply.github.com>
2022-11-03 09:04:06 -0700

    Updating text in SecondRoom.java to fit the rest of the rooms

--- src/com/shs/levelThree/SecondRoom3.java
+++ src/com/shs/levelThree/SecondRoom3.java
@@ -10,10 +10,10 @@ public class SecondRoom3 extends RoomTemplate {
 
     public SecondRoom3() {
         super(
-                "System.out.print(\"This is the second room.\")",
-                "System.out.print(\"This room is very bright although you don't see a light source. You see a passage heading east.\")"
+                "This is the second room.",
+                "This room is very bright although you don't see a light source. You see a passage heading east."
         );
-        setMoveErrorMessage("setMoveErrorMessage(\"You touch the wall and it doesn't budge.\")");
+        setMoveErrorMessage("You touch the wall and it doesn't budge.");
         addItem(sword);
     }
 }
