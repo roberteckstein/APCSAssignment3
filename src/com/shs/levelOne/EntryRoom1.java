987e766b3205a4537405a1b084a17d0cd0687d34
thederpcodeman <connertm24@sherwoodsd.com>
2022-11-04 12:21:40 -0700

    re implemented hidden room hint in entry room move error

--- src/com/shs/levelOne/EntryRoom1.java
+++ src/com/shs/levelOne/EntryRoom1.java
@@ -5,7 +5,7 @@ import com.shs.RoomTemplate;
 public class EntryRoom1 extends RoomTemplate {
     public EntryRoom1() {
         super("You are in a small cavern with a passage traveling north.", "You are in a small cavern with a passage traveling north. Your adventure starts here.");
-        setMoveErrorMessage("The cavern's walls are solid.");
+        setMoveErrorMessage("The cavern's walls are solid. Right?");
 
 
         // Add items/creatures here (watch out for duplicate items across rooms)
