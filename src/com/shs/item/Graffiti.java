7dba03241906eacb6818793d581cec1ca740997d
Archnitest <114258594+Archnitest@users.noreply.github.com>
2022-11-03 20:04:41 -0700

    More cat riddles

--- src/com/shs/item/Graffiti.java
+++ src/com/shs/item/Graffiti.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Graffiti extends RandomItemTemplate {
     public Graffiti (String color) {
         super ("graffiti","", " How did you get this?");
-       setDescription("There is " + color + " graffiti on the wall. It reads \"" + ranRiddle(numGen(19))+ "\" ");
+       setDescription("There is " + color + " graffiti on the wall. It reads \"" + ranRiddle(numGen(21))+ "\" ");
     }
 
     private String ranRiddle (int x) {
@@ -26,11 +26,13 @@ public class Graffiti extends RandomItemTemplate {
             case 11 : return "3120 B.C. The war between us and the drooling barbarians was declared. We tied, but we like to think we won.";
             case 12 : return "Basket is our god... Wait. You spelled it wrong Felix!";
             case 13 : return "Seafood. Yummy. A nice tuna fillet would be nice right now.";
-            case 14 : return "The Milky Way is where we thrive.";
+            case 14 : return "The Milky Way is what we want.";
             case 15 : return "No luck? You might've seen my cousin yesterday.";
             case 16 : return "Hi! This isn't a hint. Just an Easter Egg. Derek was here. ";
             case 17 : return "Nyan Nyan Nyan Nyan Nyan Nyan Nyan Nyan Nyan Nyan Nyan Nyan...";
             case 18 : return "Imagine living only one life.";
+            case 19 : return "I can play a piano very well. I became viral on YouTube for it.";
+            case 20 : return "Remember, you're only alive because you have paid your taxes to your ruler.";
             default : return "It broke.";
         }
     }
